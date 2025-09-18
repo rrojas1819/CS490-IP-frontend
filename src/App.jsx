389b9052d3cc23e8531eab0adf24d0ce64e30826
Richie Rojas <rrojas181920@gmail.com>
2025-09-17 21:41:39 -0400

    Set up general selection contanier!
    Container now has proper working CSS for the most part and is the skeleton, and is made in a way for the DB to populate!

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,36 @@ import { useState } from 'react'
 import Header from './components/Header'
 import './styles/App.css'
 import MovieCard from './components/MovieCard'
+import './styles/Selection.css'
 function App() {
-  
-
+  const categories = ['Long', 'Short', 'Rated One'] //Examples
+  const movies = [
+    { title: 'Movie 1', imageUrl: 'https://via.placeholder.com/150' },
+    { title: 'Movie 2', imageUrl: 'https://via.placeholder.com/150' },
+    { title: 'Movie 3', imageUrl: 'https://via.placeholder.com/150' },
+  ]
   return (
     <>
       <Header />
-      <div className='movieCardRatedOne'>
-        <MovieCard movie={"Numebr one rated Movie!"} />
+      <div className='movieCardRatedOneContainer'>
+        <div className='movieCardContainerRatedOne'>
+          <MovieCard movie={"Number one rated Movie!"} />
+        </div>
       </div>
-      <div className='movieCardContainer'>
-        
-      </div>
+      
+        {categories.map((category, index) => (
+          <div className='selectionContainer' key={index}>
+            <h1 className='movieCardCategory'>{category}</h1>
+            <div className='movieCardContainer'>
+
+              {movies.map((movie, index) => (
+                <MovieCard movie={movie} key={index} />
+              ))}
+            </div>
+            
+          </div>
+        ))}
+
     </>
   )
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,21 @@
+import '../styles/MovieCard.css'
 
-
-function MovieCard({movie}) {
-
+function MovieCard({ movie }) {
     return (
-        <div>
-            <div>{movie}</div>
+      <div className="movieCard">
+        <div className="movieCard__media">
+          {movie.imageUrl && (
+            <img className="movieCard__img" src={movie.imageUrl} alt={movie.title} />
+          )}
         </div>
-    )
-}
-
-export default MovieCard
+  
+        <div className="movieCard__body">
+          <h4 className="movieCard__title">{movie.title}</h4>
+          <span className="movieCard__kebab" aria-hidden="true" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default MovieCard;
+  
